087c6d176eb5967b58c6122957e8d5be86a282ef
Laborat-rio5 <wfbelarmino@gmail.com>
2022-01-07 15:12:21 -0300

    criação do Domain e Data

---
+++ AR.Domain/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace AR.Domain
+{
+    public class Cliente
+    {
+        [Key]
+        public Guid Id { get; set; } 
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public string Idade { get; set; }
+
+    }
+}
